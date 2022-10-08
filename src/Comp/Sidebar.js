7465ccbbe8e09bffb461ee4d42fb5da8dfc4bee0
Akash rawal <rawalakash37@gmail.com>
2022-10-08 13:37:02 +0530

    dd]

--- src/Comp/Sidebar.js
+++ src/Comp/Sidebar.js
@@ -1,26 +1,35 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import './../App.css';
 
 const Sidebar = () => {
+    const locate = useLocation().pathname
+    console.log(locate)
+    const data = {
+        name : "100nu",
+        Lname : "rawal"
+    }
     return (
         <div>
-            <div className='border shadow'>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to={`/`} ><i class="fa-solid fa-a text-info"></i></Link>
+            <div className=' shadow'>
+                <nav className='sides'>
+                    <ul className='ps-0'>
+                        <li className={locate === "/" ? "active ps-3" : "ps-3"}>
+                            <Link to={"/"}  ><i className="fa- fa-home text-info"></i>Home</Link>
                         </li>
-                        <li>
-                            <Link to={`/view`}>View</Link>
+                        <li className={locate === "/view" ? "active ps-3" : "ps-3"}>
+                            <Link className={'link'} to= "/view">
+                                View
+                            </Link>
                         </li>
-                        <li>
-                            <Link to={`/side`} className="">blacklist</Link>
+                        <li className={locate === "/blacklist" ? "active ps-3" : "ps-3"}>
+                            <Link to={`/blacklist`} className="link">blacklist</Link>
                         </li>
-                        <li>
-                            <Link to={`/side`} className="">SpamWord</Link>
+                        <li className={locate === "/SpamWord" ? "active ps-3" : "ps-3"}>
+                            <Link to={`/SpamWord`} className="link">SpamWord</Link>
                         </li>
-                        <li>
-                            <Link to={`/side`} className="">Sidebar</Link>
+                        <li className={locate === "/Sidebar" ? "active ps-3" : "ps-3"}>
+                            <Link to={`/Sidebar`} className="link">Sidebar</Link>
                         </li>
                     </ul>
                 </nav>
